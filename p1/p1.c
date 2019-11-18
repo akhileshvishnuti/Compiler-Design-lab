75abc287d25262b114420b5e92346c14c41bfcb1
AKHILESH VISHNU T I <32368189+akhileshvishnuti@users.noreply.github.com>
2019-11-18 21:44:51 +0530

    Update p1.c

--- p1/p1.c
+++ p1/p1.c
@@ -22,60 +22,62 @@ fclose(f1);*/
 f1=fopen("input","r");
 f2=fopen("identifier","w");
 f3=fopen("specialchar","w");				//25
-while((c=getc(f1))!=EOF) {
-if(isdigit(c))
+while((c=getc(f1))!=EOF) 
 {
-tokenvalue=c-'0';
-c=getc(f1);
-while(isdigit(c)){
-tokenvalue*=10+c-'0';
-c=getc(f1);
-}
-num[i++]=tokenvalue;				//35
-ungetc(c,f1);
-}
-else
-if(isalpha(c))
-{
-putc(c,f2);
-c=getc(f1);
-while(isdigit(c)||isalpha(c)||c=='_'||c=='$')
-{
-putc(c,f2);
-c=getc(f1);
-}
-putc(' ',f2);
-ungetc(c,f1);
-}					//50
-else
-if(c==' '||c=='\t')
-printf(" ");
-else
-if(c=='\n')
-lineno++;
-else
-putc(c,f3);
-}
-fclose(f2);
-fclose(f3);
-fclose(f1);
-printf("\nThe no's in the program are");
-for(j=0;j<i;j++)
-printf("%d",num[j]);
-printf("\n");
-f2=fopen("identifier","r");			//67
-k=0;
-printf("The keywords and identifiersare:");
-while((c=getc(f2))!=EOF)
-{
-if(c!=' ')
-str[k++]=c;
-else
-{
-str[k]='\0';
-keyword(str);
-k=0;
-}			//79
+ if(isdigit(c))
+ {
+  tokenvalue=c-'0';
+  c=getc(f1);
+  while(isdigit(c))
+  {
+   tokenvalue*=10+c-'0';
+   c=getc(f1);
+  }
+  num[i++]=tokenvalue;				//35
+  ungetc(c,f1);
+ }
+ else
+  if(isalpha(c))
+  {
+   putc(c,f2);
+   c=getc(f1);
+   while(isdigit(c)||isalpha(c)||c=='_'||c=='$')
+   {
+    putc(c,f2);
+    c=getc(f1);
+   }
+   putc(' ',f2);
+   ungetc(c,f1);
+  }					//50
+  else
+   if(c==' '||c=='\t')
+    printf(" ");
+   else
+    if(c=='\n')
+     lineno++;
+    else
+     putc(c,f3);
+ }
+ fclose(f2);
+ fclose(f3);
+ fclose(f1);
+ printf("\nThe no's in the program are");
+ for(j=0;j<i;j++)
+  printf("%d",num[j]);
+ printf("\n");
+ f2=fopen("identifier","r");			//67
+ k=0;
+ printf("The keywords and identifiersare:");
+ while((c=getc(f2))!=EOF)
+ {
+ if(c!=' ')
+  str[k++]=c;
+ else
+ {
+  str[k]='\0';
+  keyword(str);
+  k=0;
+ }			//79
 }
 fclose(f2);
 f3=fopen("specialchar","r");
